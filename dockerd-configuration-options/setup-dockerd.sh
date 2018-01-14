abd3a9af4e5d6a68c71eefa4294ae9cd070fdd9f
BradleyA <uadmin@rpi3b-four.cptx86.com>
2018-01-14 16:57:38 -0600

    change cptx86 search string back to Custom dockerd Configuration File

--- dockerd-configuration-options/setup-dockerd.sh
+++ dockerd-configuration-options/setup-dockerd.sh
@@ -20,7 +20,7 @@
 #
 WORK_DIRECTORY="/etc/docker/"
 UPSTART_SYSVINIT_DIRECTORY="/etc/default/"
-CONFIGURATION_STRING="cptx86"
+CONFIGURATION_STRING="Custom dockerd Configuration File"
 #
 echo -e "\n${0} [INFO]:	Changes made to ${WORK_DIRECTORY}dockerd-configuration-file will be copied to the correct locations.\n"	1>&2
 #	Must be root to run this script
