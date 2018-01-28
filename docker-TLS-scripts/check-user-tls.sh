6bde0fbe2e39124dac7ce3252302b4b311386090
BradleyA <uadmin@four-rpi3b.cptx86.com>
2018-01-28 15:26:09 -0600

    add section to check file permissions and correct if not correct ${USERHOME}${TLSUSER}/.docker

--- docker-TLS-scripts/check-user-tls.sh
+++ docker-TLS-scripts/check-user-tls.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+#	check-user-tls.sh	1.1	2018-01-28_15:25:06_CST uadmin four-rpi3b.cptx86.com
+#	add section to check file permissions and correct if not correct ${USERHOME}${TLSUSER}/.docker
 #	check-user-tls.sh	1.0	2018-01-25_21:46:22_CST uadmin rpi3b-four.cptx86.com
 #	change logic to allow user to check own certs without being root
 #
 #	set -x
 #	set -v
 #
-#	View public and private key and CA for user
+#	Check public, private keys, and CA for a user
+#	This script uses two arguements;
+#		TLSUSER - user, default is user running script
+#		USERHOME - location of user home directory, default is /home/
+#			Many sites have different home directories locations (/u/north-office/<user>)
 #
 ###
 TLSUSER=$1
-#	Where home directories are created. Some sites have different home directories (/u/north-office/<user>)
-USERHOME=/home/
+USERHOME=${2:-/home/}
 #	Check if user is entered as parameter
 if ! [ -z ${TLSUSER} ] ; then
 #       Root is required to check other users or user can check own certs
@@ -51,8 +56,28 @@ echo -e "If only one line of output is returned then the public key matches the 
 #	Verify that user certificate was issued by the CA.
 echo    "Verify that user certificate was issued by the CA."
 openssl verify -verbose -CAfile ${USERHOME}${TLSUSER}/.docker/ca.pem ${USERHOME}${TLSUSER}/.docker/cert.pem
+#	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker/ca.pem
+if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/ca.pem) != 444 ]; then
+	echo -e "${0} ${LINENO} [ERROR]:	File permissions for ${USERHOME}${TLSUSER}/.docker/ca.pem\n\tare not 444.  Correcting file permissions"	1>&2
+	chmod 0444 ${USERHOME}${TLSUSER}/.docker/ca.pem
+fi
+#	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker/cert.pem
+if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/cert.pem) != 444 ]; then
+	echo -e "${0} ${LINENO} [ERROR]:	File permissions for ${USERHOME}${TLSUSER}/.docker/cert.pem\n\tare not 444.  Correcting file permissions"	1>&2
+	chmod 0444 ${USERHOME}${TLSUSER}/.docker/cert.pem
+fi
+#	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker/key.pem
+if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/key.pem) != 400 ]; then
+	echo -e "${0} ${LINENO} [ERROR]:	File permissions for ${USERHOME}${TLSUSER}/.docker/key.pem\n\tare not 400.  Correcting file permissions"	1>&2
+	chmod 0400 ${USERHOME}${TLSUSER}/.docker/key.pem
+fi
+#	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker directory
+if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker) != 700 ]; then
+	echo -e "${0} ${LINENO} [ERROR]:	File permissions for ${USERHOME}${TLSUSER}/.docker\n\tare not 700.  Correcting file permissions"	1>&2
+	chmod 700 ${USERHOME}${TLSUSER}/.docker/key.pem
+fi
 #
-#	May want to create a verstion of this script that automates this process for SRE,
+#	May want to create a version of this script that automates this process for SRE tools,
 #		but keep this script for users to run manually,
 #		but remove the root requirement from the manual version
 #		because users can view their own ~/.docker directory
