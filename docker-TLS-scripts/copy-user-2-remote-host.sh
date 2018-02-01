ccf46149c38a117cd0f4236f6cd7ccae5ab86e08
BradleyA <allen.bradley@ymail.com>
2018-02-01 12:35:30 -0600

    debug complete for admin user copy user TLS keys to remote system ~/.docker

--- docker-TLS-scripts/copy-user-2-remote-host.sh
+++ docker-TLS-scripts/copy-user-2-remote-host.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#	./copy-user-2-remote-host.sh	3.3	2018-02-01_12:34:08_CST uadmin six-rpi3b.cptx86.com
+#	debug complete for admin user copy user TLS keys to remote system ~/.docker
 #	copy-user-remote-host.sh	3.2	2018-01-25_23:23:05_CST uadmin rpi3b-four.cptx86.com
 #	changed variable names and added test login, need alot of testing
 #	copy-user-remote-host	3.1	2017-12-19_15:32:23_CST utwo two.cptx86.com
@@ -13,25 +15,23 @@
 #		REMOTEHOST - name of host to copy certificates to
 #               USERHOME - location of admin user directory, default is /home/
 #                  Many sites have different home directories (/u/north-office/<user>)
-#               ADMTLSUSER - administration user creating TLS accounts, default is user running script
+#               ADMTLSUSER - site administrator account creating TLS keys, default is user running script
+#		   site administrator will have accounts on all systems
 #		SSHPORT - SSH server port, default port 22
 #	Documentation: https://github.com/BradleyA/docker-scripts/tree/master/docker-TLS-scripts
 #
-# >>>>> This scrip 
+# >>>>> Test this script 
 #	use case 1 TLSUSER is able to copy TLS keys and CA to remote system with same user ID
 #	use case 2 uadmin is able to copy TLS keys and CA to remote system with another USER ID
 #		and chown chmod and write to remote user home directory 
-#	think about using tar to create transfer and untar then chown and chmod
-#	need a host version that copies keys and cert to remote system in secure directory to untar
 #	does the remote system have the same home directory for user on remote system
-# >>>>>	Need to test to determine how to support this for uadmin to admin@remotesystem and uadmin to <otheruser>@remotesystem
+#
+# >>>>>	Test to determine how to support this for uadmin to admin@remotesystem and uadmin to <otheruser>@remotesystem
 #	
 #	will require more logic to support all use cases and updated prompts
 #	uadmin is the account for site and all clusters
 #	may want to see how to support more than one uadmin account in clusters for this script
 #	this opens up challenge with different users needing to chown and chgrp for different remote users
-#	
-#	
 ###
 TLSUSER=${1:-${USER}}
 REMOTEHOST=$2
@@ -69,8 +69,8 @@ if [ -z ${REMOTEHOST} ] ; then
 fi
 #	check if ${REMOTEHOST} is available on port ${SSHPORT}
 if $(nc -z  ${REMOTEHOST} ${SSHPORT} >/dev/null) ; then
+	echo -e "${0} ${LINENO} [INFO]:	Create directory, change\n\tfile permissions, and copy TLS keys to ${TLSUSER}@${REMOTEHOST}."
 	cd ${USERHOME}${ADMTLSUSER}/.docker/docker-ca
-	echo -e "${0} ${LINENO} [INFO]:	Create directory, change\n\tpermissions, and copy TLS key to ${TLSUSER}@${REMOTEHOST}."
 	mkdir -p ${TLSUSER}/.docker
 	chmod 700 ${TLSUSER}/.docker
 	cp -p ca.pem ${TLSUSER}/.docker
@@ -82,16 +82,14 @@ if $(nc -z  ${REMOTEHOST} ${SSHPORT} >/dev/null) ; then
 	ln -s ${TLSUSER}-user-priv-key.pem key.pem
 	cd ..
 	tar -cf ./${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar .docker
+	echo -e "${0} ${LINENO} [INFO]:	Transfer TLS keys to ${TLSUSER}@${REMOTEHOST}."
 	scp -p  ./${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar ${ADMTLSUSER}@${REMOTEHOST}:/tmp
-#	ssh -t ${ADMTLSUSER}@${REMOTEHOST} cd ~${TLSUSER} ; sudo tar -xf /tmp/${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar ; sudo chown -R  ${TLSUSER}.${TLSUSER} .docker
-#	next line works last night
-#	ssh -t ${ADMTLSUSER}@${REMOTEHOST} " cd ~${TLSUSER} ; pwd ; hostname -f ; sudo tar -xvf /tmp/${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar ; rm /tmp/${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar ;  sudo chown -R  ${TLSUSER}.${TLSUSER} .docker "
+	echo -e "${0} ${LINENO} [INFO]:	May receive ${ADMTLSUSER} password prompt from ${REMOTEHOST}."
 	ssh -t ${ADMTLSUSER}@${REMOTEHOST} " cd ~${TLSUSER} ; sudo tar -xf /tmp/${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar ; rm /tmp/${TLSUSER}${REMOTEHOST}${TIMESTAMP}.tar ;  sudo chown -R  ${TLSUSER}.${TLSUSER} .docker "
-pwd
-###	uncomment after debugging
-#	Remove ${TLSUSER}/.docker and tar file of it
-	cd ${USERHOME}${ADMTLSUSER}/.docker/docker-ca
-#	rm -rf ${TLSUSER}
+#	Remove ${TLSUSER}/.docker and tar file from ${USERHOME}${ADMTLSUSER}/.docker/docker-ca
+	cd ..
+	rm -rf ${TLSUSER}
+	echo -e "${0} ${LINENO} [INFO]:	Done."
 ###
 else
 	echo -e "${0} ${LINENO} [ERROR]:	${REMOTEHOST} not responding on port ${SSHPORT}.\n"  1>&2
