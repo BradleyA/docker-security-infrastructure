9532234cdd4b88f0264d9604504f4f3ac337e0c8
BradleyA <allen.bradley@ymail.com>
2018-02-01 17:19:30 -0600

    added logic for display_help()

--- docker-TLS/check-host-tls.sh
+++ docker-TLS/check-host-tls.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
+#	check-host-tls.sh	3.4	2018-02-01_17:18:43_CST uadmin six-rpi3b.cptx86.com
+#	added logic for display_help()
 #	check-host-tls.sh	1.2	2018-01-28_23:17:45_CST uadmin four-rpi3b.cptx86.com
 #	tested files & dir check & correct section permissions
 #	check-host-tls.sh	1.0	2018-01-25_22:17:05_CST uadmin rpi3b-four.cptx86.com
 #	initial commit
 #
-#       set -x
-#       set -v
+#	set -x
+#	set -v
 #
-#	Check public, private keys, and CA for host
-#       This script uses one arguement;
-#               CERTDIR - dockerd certification directory, default /etc/docker/certs.d/daemon/
-#       This script run as root checks host public, private keys, and CA in /etc/docker/certs.d/daemon directory
-#       Documentation: https://github.com/BradleyA/docker-scripts/tree/master/docker-TLS-scripts
+display_help() {
+echo -e "\nCheck public, private keys, and CA for host"
+echo    "This script uses one arguement;"
+echo    "   CERTDIR - dockerd certification directory, default /etc/docker/certs.d/daemon/"
+echo    "This script has to be run as root to check host public, private keys, and\n\tCA in /etc/docker/certs.d/daemon directory."
+echo    "This script currently DOES NOT CHECK REMOTE HOSTS."
+echo -e "Documentation: https://github.com/BradleyA/docker-scripts/tree/master/docker-TLS-scripts\n"
+echo -e "Example:\tsudo ${0}\n"
+}
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
+	display_help
+	exit 0
+fi
 ### 
 CERTDIR=${1:-/etc/docker/certs.d/daemon/}
-#
-#
-# >>>>> Add systax --help -? -h -help for scripts
-#
 #       Must be root to run this script
 if ! [ $(id -u) = 0 ] ; then
-        echo "${0} ${LINENO} [ERROR]:   Use sudo ${0}"  1>&2
-        echo -e "\n>>   SCRIPT MUST BE RUN AS ROOT <<"     1>&2
-        exit 1
+	echo "${0} ${LINENO} [ERROR]:   Use sudo ${0}"  1>&2
+	echo -e "\n>>   SCRIPT MUST BE RUN AS ROOT <<"     1>&2
+	display_help
+	exit 1
 fi
 #       Check for ${CERTDIR} directory
 if [ ! -d ${CERTDIR} ] ; then
-        echo -e "${0} ${LINENO} [ERROR]:	${CERTDIR} does not exist"   1>&2
-        exit 1
+	echo -e "${0} ${LINENO} [ERROR]:	${CERTDIR} does not exist"   1>&2
+	display_help
+	exit 1
 fi
 #	View dockerd daemon certificate expiration date of ca.pem file
 echo -e "\nView dockerd daemon certificate expiration date of ca.pem file."
@@ -67,4 +75,5 @@ fi
 #
 #       May want to create a version of this script that automates this process for SRE tools,
 #               but keep this script for users to run manually,
+#	open ticket and remove this comment
 #
