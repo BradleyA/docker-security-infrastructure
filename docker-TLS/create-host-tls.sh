813db59f0ca1ac6a015c625590cb8e91018f5999
BradleyA <allen.bradley@ymail.com>
2018-02-01 19:31:51 -0600

    added logic for display_help()

--- docker-TLS/create-host-tls.sh
+++ docker-TLS/create-host-tls.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#	create-host-tls.sh	3.4	2018-02-01_19:31:40_CST uadmin six-rpi3b.cptx86.com
+#	added logic for display_help()
 #	create-host-tls.sh	3.3	2018-01-31_09:08:41_CST uadmin six-rpi3b.cptx86.com
 #	Improve user feed back messages
 #	create-host-tls.sh	3.2	2018-01-31_07:18:02_CST uadmin six-rpi3b.cptx86.com
@@ -9,24 +11,31 @@
 #	set -x
 #	set -v
 #
-#	Create public and private key and CA for host
-#	This script uses two arguements;
-#		FQDN - Fully qualified domain name of host requiring new TLS keys
-#		NUMBERDAYS - number of days host CA is valid, default 365 days
-#	This script creates public and private key and CA in working directory,${HOME}/.docker/docker-ca.
-#	Documentation: https://github.com/BradleyA/docker-scripts/tree/master/docker-TLS-scripts
+display_help() {
+echo -e "\nCreate public, private keys and CA for host in ${HOME}/.docker/docker-ca"
+echo    "This script uses 4 arguements;"
+echo    "   FQDN - Fully qualified domain name of host requiring new TLS keys"
+echo    "   NUMBERDAYS - number of days host CA is valid, default 365 days"
+echo    "   USERHOME - location of admin user directory, default is /home/"
+echo    "      Many sites have different home directories (/u/north-office/<user>)"
+echo    "   ADMTLSUSER - site administrator account creating TLS keys, default is user running script"
+echo    "      site administrator will have accounts on all systems"
+echo -e "Documentation: https://github.com/BradleyA/docker-scripts/tree/master/docker-TLS-scripts\n"
+echo -e "Example::\t${0} two.cptx86.com 180 /u/north-office/ uadmin\n"
+}
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
+	display_help
+	exit 0
+fi
 ###		
 FQDN=$1
 NUMBERDAYS=${2:-365}
 USERHOME=${3:-/home/}
 ADMTLSUSER=${4:-${USER}}
-#
-#
-# >>>>> Add systax --help -? -h -help for scripts
-#
 #	Check if admin user has home directory on system
 if [ ! -d ${USERHOME}${ADMTLSUSER} ] ; then
 	echo -e "${0} ${LINENO} [ERROR]:        ${ADMTLSUSER} does not have a home directory\n\ton this system or ${ADMTLSUSER} home directory is not ${USERHOME}${ADMTLSUSER}"  1>&2
+	display_help
 	exit 1
 fi
 #       Check if site CA directory on system
@@ -37,6 +46,7 @@ if [ ! -d ${USERHOME}${ADMTLSUSER}/.docker/docker-ca/.private ] ; then
 	echo -e "\tand site private and public keys.  Then run sudo"
 	echo -e "\tcreate-new-openssl.cnf-tls.sh to modify openssl.cnf file.  Then run"
 	echo -e "\tcreate-host-tls.sh or create-user-tls.sh as many times as you want."
+	display_help
 	exit 1
 fi
 #
@@ -48,6 +58,7 @@ if ! [ -e ${USERHOME}${ADMTLSUSER}/.docker/docker-ca/.private/ca-priv-key.pem ] 
 	echo -e "\t${USERHOME}${ADMTLSUSER}/.docker/docker-ca/.private/"
 	echo -e "\tOr run create-site-private-public-tls.sh and sudo"
 	echo -e "\tcreate-new-openssl.cnf-tls.sh to create a new one."
+	display_help
 	exit 1
 fi
 #	Prompt for ${FQDN} if argement not entered
@@ -58,6 +69,7 @@ fi
 #	Check if ${FQDN} string length is zero
 if [ -z ${FQDN} ] ; then
 	echo -e "${0} ${LINENO} [ERROR]:	A Fully Qualified Domain Name\n\t(FQDN) is required to create new host TLS keys."	1>&2
+	display_help
 	exit 1
 fi
 #	Check if ${FQDN}-priv-key.pem file exists
@@ -80,28 +92,5 @@ echo -e "${0} ${LINENO} [INFO]:	Removing certificate signing requests\n\t(CSR) a
 rm ${FQDN}.csr
 chmod 0400 ${FQDN}-priv-key.pem
 chmod 0444 ${FQDN}-cert.pem
-echo -e "${0} ${LINENO} [INFO]:	Instructions for setting up\n\tpublic, private, and certificate files for ${FQDN}.\n"	1>&2
-echo    "Login to host ${FQDN} and create a Docker daemon TLS directory.  "
-echo    "		ssh <admin-user>@${FQDN}"
-echo    "		sudo mkdir -p /etc/docker/certs.d/daemon"
-echo    "Change the directory permission for the Docker daemon TLS directory on\n\thost ${FQDN} and logout."
-echo    "		sudo chmod 0700 /etc/docker/certs.d/daemon"
-echo    "		logout"
-echo    "Copy the keys from working directory to /tmp directory\n\ton host ${FQDN}."
-echo    "		scp ./ca.pem <admin-user>@${FQDN}:'/tmp/ca.pem'"
-echo    "		scp ./${FQDN}-cert.pem <admin-user>@${FQDN}:'/tmp/${FQDN}-cert.pem'"
-echo    "		scp ./${FQDN}-priv-key.pem <admin-user>@${FQDN}:'/tmp/${FQDN}-priv-key.pem'"
-echo    "Login to ${FQDN} and move key pair files from the /tmp directory\n\tto Docker daemon TLS directory."
-echo    "		ssh <admin-user>@${FQDN}"
-echo    "		cd /tmp"
-echo    "		sudo mv *.pem /etc/docker/certs.d/daemon"
-echo    "		sudo chown -R root.root /etc/docker/certs.d/daemon"
-echo    "		sudo -i"
-echo    "		cd /etc/docker/certs.d/daemon"
-echo    "		ln -s ${FQDN}-cert.pem cert.pem"
-echo    "		ln -s ${FQDN}-priv-key.pem key.pem"
-echo    "		exit"
-echo -e "Add TLS flags to dockerd so dockerd will know you are using TLS.\n\t(--tlsverify, --tlscacert, --tlscert, --tlskey)\n"
-echo -e "Scripts to help with setup and operaations of Docker and TLS:\n\thttps://github.com/BradleyA/docker-scripts/tree/master/dockerd-configuration-options"
-echo -e "\tThese dockerd-configuration-options scripts will help with\n\tconfiguration of dockerd on systems running Ubuntu 16.04\n\t(systemd) and Ubuntu 14.04 (Upstart)."
+echo -e "${0} ${LINENO} [INFO]:	Done.\n"	1>&2
 ###
