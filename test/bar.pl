f2a77558b8df0377ac869752cf8d2cca243f5b1a
BradleyA <allen.bradley@ymail.com>
2018-02-13 21:40:04 -0600

    before 3.6-13-g4857961 Current release number: 3.6

--- test/bar.pl
+++ test/bar.pl
@@ -1,7 +1,8 @@
 ########################
+# 	bar.pl	3.6.280	2018-02-13_21:39:19_CST uadmin six-rpi3b.cptx86.com 3.6-13-g4857961 
+# 	   before 3.6-13-g4857961 Current release number: 3.6 
+# 	bar.pl	3.6.280	2018-02-13_21:32:41_CST uadmin six-rpi3b.cptx86.com 3.6-13-g4857961 
+# 	   before 3.6-13-g4857961  3.6 after 
 # 	bar.pl	3.7.278	2018-02-13_21:14:05_CST uadmin six-rpi3b.cptx86.com 3.6-11-g9ec13a9 
 # 	   before v0.1 3.6-11-g9ec13a9 after 3.7 
-
-
-
 ###
--- test/foo.c
+++ test/foo.c
@@ -1,7 +1,8 @@
 ################
+/* 	foo.c	3.6.280	2018-02-13_21:39:19_CST uadmin six-rpi3b.cptx86.com 3.6-13-g4857961  */
+/* 	   before 3.6-13-g4857961 Current release number: 3.6  */
+/* 	foo.c	3.6.280	2018-02-13_21:32:41_CST uadmin six-rpi3b.cptx86.com 3.6-13-g4857961  */
+/* 	   before 3.6-13-g4857961  3.6 after  */
 /* 	foo.c	3.6.277	2018-02-13_21:11:35_CST uadmin six-rpi3b.cptx86.com 3.6-10-g3344215  */
 /* 	   before 3.6-10-g3344215  */
-
-
-
 ###
