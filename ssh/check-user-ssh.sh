663b5465ef4cd1658eda90a686744c1355b669c1
BradleyA <allen.bradley@ymail.com>
2018-03-04 13:42:31 -0600

    completed debug process, ready for production

--- ssh/check-user-ssh.sh
+++ ssh/check-user-ssh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	check-user-ssh.sh  3.17.323  2018-03-04_13:42:20_CST  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.16  
+# 	   completed debug process, ready for production 
 # 	check-user-ssh.sh  3.16.322  2018-03-04_13:24:51_CST  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.15-3-gd1f73f0  
 # 	   initial commit for check-user-ssh.sh 
 #
@@ -67,11 +69,11 @@ if [ ! -e ${USERHOME}${TLSUSER}/.ssh/id_rsa.pub ] ; then
 	echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  ${TLSUSER} does not have a .ssh/id_rsa.pub file"	1>&2
 	exit 1
 fi
-#	Check if a RSA public / private key pair matche
+#	Check if a SSH public / private key pair matche
 echo -e "\n${NORMAL}Check if ${USERHOME}${TLSUSER}/.ssh/id_rsa matches ${USERHOME}${TLSUSER}/.ssh/id_rsa.pub."
-diff -qs <(ssh-keygen -y -f ${USERHOME}${TLSUSER}/.ssh/id_rsa) <(cut -d ' ' -f 1,2 ${USERHOME}${TLSUSER}/ssh/id_rsa.pub)
+diff -qs <(ssh-keygen -y -f ${USERHOME}${TLSUSER}/.ssh/id_rsa) <(cut -d ' ' -f 1,2 ${USERHOME}${TLSUSER}/.ssh/id_rsa.pub)
 #	Verify and correct directory permissions for ${USERHOME}${TLSUSER}/.ssh
-echo    "Verify and correct directory permissions for ${USERHOME}${TLSUSER}/.ssh"
+echo    "Verify and correct file and directory permissions for ${USERHOME}${TLSUSER}/.ssh"
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh) != 700 ]; then
 	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  Directory permissions for ${USERHOME}${TLSUSER}/.ssh\n\tare not 700.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh) to 0700 directory permissions"	1>&2
 	chmod 0700 ${USERHOME}${TLSUSER}/.ssh
@@ -79,7 +81,7 @@ fi
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.ssh/id_rsa
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/id_rsa) != 600 ]; then
 	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  File permissions for ${USERHOME}${TLSUSER}/.ssh/id_rsa\n\tare not 600.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/id_rsa) to 0600 file permissions"	1>&2
-	chmod 0600 ${USERHOME}${TLSUSER}/.shh/id_rsa
+	chmod 0600 ${USERHOME}${TLSUSER}/.ssh/id_rsa
 fi
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.ssh/id_rsa.pub
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/id_rsa.pub) != 644 ]; then
@@ -89,12 +91,12 @@ fi
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.ssh/authorized_keys
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/authorized_keys) != 600 ]; then
 	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  File permissions for ${USERHOME}${TLSUSER}/.ssh/authorized_keys\n\tare not 600.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/authorized_keys) to 600 file permissions"	1>&2
-	chmod 700 ${USERHOME}${TLSUSER}/.ssh/authorized_keys
+	chmod 600 ${USERHOME}${TLSUSER}/.ssh/authorized_keys
 fi
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.ssh/known_hosts
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/known_hosts) != 644 ]; then
 	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  File permissions for ${USERHOME}${TLSUSER}/.ssh/known_hosts\n\tare not 644.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.ssh/known_hosts) to 644 file permissions"	1>&2
-	chmod 700 ${USERHOME}${TLSUSER}/.ssh/known_hosts
+	chmod 644 ${USERHOME}${TLSUSER}/.ssh/known_hosts
 fi
 echo -e "\n${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	Done.\n"	1>&2
 #
