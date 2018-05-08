2f35230e829e484481909510b77c1aad7d54037d
BradleyA <allen.bradley@ymail.com>
2018-05-08 10:11:53 -0500

    cleanup help page

--- dockerd-configuration-options/uninstall-dockerd-scripts.sh
+++ dockerd-configuration-options/uninstall-dockerd-scripts.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# 	uninstall-dockerd-scripts.sh  3.23.336  2018-05-08_07:39:15_CDT  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.22  
-# 	   finished upstart section 
-# 	uninstall-dockerd-scripts.sh  3.22.335  2018-05-07_22:26:50_CDT  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.21  
-# 	   add exit line while working on script 
-# 	uninstall-dockerd-scripts.sh  3.21.334  2018-05-07_11:08:06_CDT  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.20-6-g270eb14  
-# 	   Initial submit 
+# 	uninstall-dockerd-scripts.sh  3.24.337  2018-05-08_10:11:37_CDT  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.23  
+# 	   cleanup help page 
 #
 #	set -v
 #	set -x
@@ -13,25 +9,14 @@ display_help() {
 echo -e "\n${0} - uninstall scripts that support dockerd on Systemd and Upstart."
 echo -e "\nUSAGE\n   sudo ${0} "
 echo    "   sudo ${0} [--help | -help | help | -h | h | -? | ?] [--version | -v]"
-echo -e "\nDESCRIPTION\nThis script has to be run as root to remove files from /etc/docker and create"
-echo    "XXXXX xxxxx or update the /etc/systemd/system/docker.service.d/10-override.conf"
-echo    "file (Ubuntu 16.04, systemd) and the /etc/default/docker (Ubuntu"
-echo    "14.04, Upstart). To change the docker daemon flags, sudo edit"
-echo    "/etc/docker/dockerd-configuration-file and run sudo"
-echo    "/etc/docker/setup-dockerd.sh. Docker daemon flag changes can be"
-echo    "distributed to any Ubuntu cluster that use systemd or upstart by"
-echo    "copying /etc/docker/dockerd-configuration-file to each system and"
-echo    "running sudo /etc/docker/setup-dockerd.sh on each system."
+echo -e "\nDESCRIPTION\nThis script has to be run as root to remove scripts and files from /etc/docker"
+echo    "and /etc/systemd/system."
 echo -e "\nOPTIONS"
 echo    "   WORK_DIRECTORY               working directory,"
 echo    "                                default is /etc/docker/"
 echo    "   UPSTART_SYSVINIT_DIRECTORY   Ubuntu 14.04 (Upstart) directory,"
 echo    "                                default is /etc/default/"
-#	echo    "   CONFIGURATION_STRING   >> no use case currently, future development <<<"
-#	echo    "                          default is Custom_dockerd_Configuration_File"
 echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/docker-scripts/tree/master/dockerd-configuration-options"
-echo -e "\nEXAMPLES\n   After editing /etc/docker/dockerd-configuration-file, run"
-echo -e "\tsudo ${0}\n"
 }
 if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
 	display_help
@@ -54,16 +39,14 @@ if ! [ $(id -u) = 0 ] ; then
 	echo "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	Use sudo ${0}"	1>&2
 	exit 1
 fi
-echo "THis is not complete.  Need a few days" ; exit 1
+#
+###	Remove dockerd (Upstart and SysVinit configuration file) on Ubuntu 14.04
+#
 #	Check for ${WORK_DIRECTORY}
 if [ ! -d ${WORK_DIRECTORY} ] ; then
 	echo "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	Is Docker installed?  Directory ${WORK_DIRECTORY} not found."	1>&2
-	exit 1
-
- # >>> #	may want to continue even if /etc/docker is missing because the files are in other directories!
-
 elif [ ! -f ${WORK_DIRECTORY}setup-dockerd.sh ] ; then
-#	Removing files from ${WORK_DIRECTORY} if not already removed
+#	Removing files from ${WORK_DIRECTORY}
 	echo "${NORMAL}${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	Removing files from ${WORK_DIRECTORY}."	1>&2
 	rm -f ${WORK_DIRECTORY}10-override.begin
 	rm -f ${WORK_DIRECTORY}dockerd-configuration-file
@@ -73,31 +56,26 @@ elif [ ! -f ${WORK_DIRECTORY}setup-dockerd.sh ] ; then
 	rm -f ${WORK_DIRECTORY}start-dockerd-with-systemd.begin
 	rm -f ${WORK_DIRECTORY}start-dockerd-with-systemd.end
 	rm -f ${WORK_DIRECTORY}start-dockerd-with-systemd.sh
-fi
 #	Check for dockerd configuration file
-if [ -f ${UPSTART_SYSVINIT_DIRECTORY}docker ] ; then
+	if [ -f ${UPSTART_SYSVINIT_DIRECTORY}docker ] ; then
 #	copy ${WORK_DIRECTORY}docker.org to ${UPSTART_SYSVINIT_DIRECTORY}docker
-        cp   ${WORK_DIRECTORY}docker.org    ${UPSTART_SYSVINIT_DIRECTORY}docker
+        	cp   ${WORK_DIRECTORY}docker.org    ${UPSTART_SYSVINIT_DIRECTORY}docker
+	else
+		echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}INFO${NORMAL}]:      ${UPSTART_SYSVINIT_DIRECTORY}docker not found."
+	fi
+	rm -f ${WORK_DIRECTORY}docker.org
 fi
-rm -f ${WORK_DIRECTORY}docker.org
 echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	If you are using upstart, Run\n\t'${BOLD}sudo service docker restart${NORMAL}' for dockerd to read ${UPSTART_SYSVINIT_DIRECTORY}docker.\n"	1>&2
 #
-###	Configure dockerd (systemd) on Ubuntu 16.04
-#		Any changes to dockerd-configuration-file will be added to ${WORK_DIRECTORY}${START_SYSTEMD_SCRIPT}
+###	Remove dockerd (systemd) on Ubuntu 16.04
 #
-START_SYSTEMD_SCRIPT="start-dockerd-with-systemd.sh"
+#	Remove /etc/systemd/system/dockerd-configuration-file.service 
+rm -f /etc/systemd/system/dockerd-configuration-file.service
+#	Remove directories docker.service.d and docker.service.wants
+rm -rf /etc/systemd/system/docker.service.d
+rm -rf /etc/systemd/system/docker.service.wants
 #
-echo -e "${NORMAL}${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	Update files for dockerd (systemd configuration\n\tfile) on Ubuntu 16.04."	1>&2
-cat ${WORK_DIRECTORY}start-dockerd-with-systemd.begin > ${WORK_DIRECTORY}${START_SYSTEMD_SCRIPT}
-cat ${WORK_DIRECTORY}dockerd-configuration-file >> ${WORK_DIRECTORY}${START_SYSTEMD_SCRIPT}
-cat ${WORK_DIRECTORY}start-dockerd-with-systemd.end >> ${WORK_DIRECTORY}${START_SYSTEMD_SCRIPT}
-chmod 700 ${WORK_DIRECTORY}${START_SYSTEMD_SCRIPT}
-${WORK_DIRECTORY}${START_SYSTEMD_SCRIPT}
-#	Add /etc/systemd/system/dockerd-configuration-file.service to systemd
-cp ${WORK_DIRECTORY}/dockerd-configuration-file.service /etc/systemd/system/
 systemctl daemon-reload
 #
-echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	If you are using systemd, Run\n\t'${BOLD}sudo systemctl enable dockerd-configuration-file.service${NORMAL}'\n\tto start on boot."	1>&2
-echo -e "${NORMAL}${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	Run '${BOLD}sudo systemctl enable docker${NORMAL}'\n\tto start on boot."	1>&2
 echo    "${NORMAL}${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	Run '${BOLD}sudo systemctl restart docker${NORMAL}'"	1>&2
 ###
