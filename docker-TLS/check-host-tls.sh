a93abae486db2c440bb7c0fb75c54a7b0bada874
Bradley Allen <allen.bradley@ymail.com>
2018-10-22 11:29:01 -0500

    check-user-tls.sh Change echo or print DEBUG INFO WARNING ERROR close #20

--- docker-TLS/check-host-tls.sh
+++ docker-TLS/check-host-tls.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	docker-TLS/check-host-tls.sh  3.64.421  2018-10-22T11:29:01.566445-05:00 (CDT)  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.63  
+# 	   check-user-tls.sh Change echo or print DEBUG INFO WARNING ERROR close #20 
 # 	docker-TLS/check-host-tls.sh  3.63.420  2018-10-22T10:27:30.804074-05:00 (CDT)  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.62  
 # 	   check-host-tls.sh Change echo or print DEBUG INFO WARNING ERROR close #19 
 # 	check-host-tls.sh  3.35.374  2018-08-05_23:21:09_CDT  https://github.com/BradleyA/docker-scripts  uadmin  three-rpi3b.cptx86.com 3.34  
@@ -8,8 +10,9 @@
 DEBUG=0                 # 0 = debug off, 1 = debug on
 #	set -x
 #	set -v
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
+BOLD=$(tput -Txterm bold)
+NORMAL=$(tput -Txterm sgr0)
+###
 display_help() {
 echo -e "\n${NORMAL}${0} - Check public, private keys, and CA for host"
 echo -e "\nUSAGE\n   sudo ${0} [CERTDIR]"
@@ -94,47 +97,58 @@ if [ ! -d ${CERTDIR} ] ; then
 	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  ${CERTDIR} does not exist" 1>&2
 	exit 1
 fi
+
 get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[INFO]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Checking ${BOLD}${REMOTEHOST}${NORMAL}TLS certifications and directory permissions." 1>&2
+
 #	View dockerd daemon certificate expiration date of ca.pem file
 TEMP=`openssl x509 -in ${CERTDIR}ca.pem -noout -enddate`
 echo -e "\nView dockerd daemon certificate expiration date of ca.pem file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	View dockerd daemon certificate expiration date of cert.pem file
 TEMP=`openssl x509 -in ${CERTDIR}cert.pem -noout -enddate`
 echo -e "\nView dockerd daemon certificate expiration date of cert.pem file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	View dockerd daemon certificate issuer data of the ca.pem file
 TEMP=`openssl x509 -in ${CERTDIR}ca.pem -noout -issuer`
 echo -e "\nView dockerd daemon certificate issuer data of the ca.pem file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	View dockerd daemon certificate issuer data of the cert.pem file
 TEMP=`openssl x509 -in ${CERTDIR}cert.pem -noout -issuer`
 echo -e "\nView dockerd daemon certificate issuer data of the cert.pem file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	Verify that dockerd daemon certificate was issued by the CA.
 TEMP=`openssl verify -verbose -CAfile ${CERTDIR}ca.pem ${CERTDIR}cert.pem`
 echo -e "\nVerify that dockerd daemon certificate was issued by the CA:\n\t${BOLD}${TEMP}${NORMAL}"
-#
+
 echo -e "\nVerify and correct file permissions."
+
 #	Verify and correct file permissions for ${CERTDIR}ca.pem
 if [ $(stat -Lc %a ${CERTDIR}ca.pem) != 444 ]; then
 	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  File permissions for ${CERTDIR}ca.pem are not 444.  Correcting $(stat -Lc %a ${CERTDIR}ca.pem) to 0444 file permissions" 1>&2
 	chmod 0444 ${CERTDIR}ca.pem
 fi
+
 #	Verify and correct file permissions for ${CERTDIR}cert.pem
 if [ $(stat -Lc %a ${CERTDIR}cert.pem) != 444 ]; then
 	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  File permissions for ${CERTDIR}cert.pem are not 444.  Correcting $(stat -Lc %a ${CERTDIR}cert.pem) to 0444 file permissions" 1>&2
 	chmod 0444 ${CERTDIR}cert.pem
 fi
+
 #	Verify and correct file permissions for ${CERTDIR}key.pem
 if [ $(stat -Lc %a ${CERTDIR}key.pem) != 400 ]; then
 	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  File permissions for ${CERTDIR}key.pem are not 400.  Correcting $(stat -Lc %a ${CERTDIR}key.pem) to 0400 file permissions" 1>&2
 	chmod 0400 ${CERTDIR}key.pem
 fi
+
 #	Verify and correct directory permissions for ${CERTDIR} directory
 if [ $(stat -Lc %a ${CERTDIR}) != 700 ]; then
 	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Directory permissions for ${CERTDIR} are not 700.  Correcting $(stat -Lc %a ${CERTDIR}) to 700 directory permissions" 1>&2
 	chmod 700 ${CERTDIR}
 fi
-#
+
+	Help hint
 echo -e "\nUse script ${BOLD}create-host-tls.sh${NORMAL} to update host TLS if host TLS certificate has expired."
-#
+ 
 #	May want to create a version of this script that automates this process for SRE tools,
 #	but keep this script for users to run manually,
 #	open ticket and remove this comment
--- docker-TLS/check-user-tls.sh
+++ docker-TLS/check-user-tls.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# 	docker-TLS/check-user-tls.sh  3.42.391  2018-08-12_10:59:20_CDT  https://github.com/BradleyA/docker-scripts  uadmin  three-rpi3b.cptx86.com 3.41-8-g21e9f27  
-# 	   sync to standard script design changes 
+# 	docker-TLS/check-user-tls.sh  3.64.421  2018-10-22T11:29:01.659220-05:00 (CDT)  https://github.com/BradleyA/docker-scripts  uadmin  six-rpi3b.cptx86.com 3.63  
+# 	   check-user-tls.sh Change echo or print DEBUG INFO WARNING ERROR close #20 
 # 	check-user-tls.sh  3.35.374  2018-08-05_23:21:09_CDT  https://github.com/BradleyA/docker-scripts  uadmin  three-rpi3b.cptx86.com 3.34  
 # 	   improve output of script close #13 
-###
+#
+###	check-user-tls.sh - Check public, private keys, and CA for a user
 DEBUG=0                 # 0 = debug off, 1 = debug on
 #	set -x
 #	set -v
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
+BOLD=$(tput -Txterm bold)
+NORMAL=$(tput -Txterm sgr0)
+###
 display_help() {
 echo -e "\n${NORMAL}${0} - Check public, private keys, and CA for a user"
 echo -e "\nUSAGE\n   ${0} [<user-name>] [<home-directoty>]"
-echo    "   ${0} [--help | -help | help | -h | h | -? | ?]"
+echo    "   ${0} [--help | -help | help | -h | h | -?]"
 echo    "   ${0} [--version | -version | -v]"
 echo -e "\nDESCRIPTION\nUsers can check their public, private keys, and CA in /home or other"
 echo    "non-default home directories.  The file and directory permissions are also"
@@ -27,102 +29,148 @@ echo -e "\nEXAMPLES\n   ${0}\n\n   User can check their certificates"
 echo -e "\n   ${0} sam /u/north-office/\n\n   User sam checks their certificates in a non-default home directory"
 echo -e "\n   sudo ${0} bob\n\n   Administrator checks user bob certificates"
 echo -e "\n   sudo ${0} sam /u/north-office/\n\n   Administrator checks user sam certificates in a different home directory"
+#       After displaying help in english check for other languages
 if ! [ "${LANG}" == "en_US.UTF-8" ] ; then
-        echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARNING${NORMAL}]:     Your language, ${LANG}, is not supported.\n\tWould you like to help?\n" 1>&2
+        get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[WARN]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Your language, ${LANG}, is not supported, Would you like to help translate?" 1>&2
+#       elif [ "${LANG}" == "fr_CA.UTF-8" ] ; then
+#               get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[WARN]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Display help in ${LANG}" 1>&2
+#       else
+#               get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[WARN]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Your language, ${LANG}, is not supported.\tWould you like to translate?" 1>&2
 fi
 }
-if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
-	display_help
-	exit 0
-fi
-if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
-        head -2 ${0} | awk {'print$2"\t"$3'}
+
+#       Date and time function ISO 8601
+get_date_stamp() {
+DATE_STAMP=`date +%Y-%m-%dT%H:%M:%S.%6N%:z`
+TEMP=`date +%Z`
+DATE_STAMP=`echo "${DATE_STAMP} (${TEMP})"`
+}
+
+#       Fully qualified domain name FQDN hostname
+LOCALHOST=`hostname -f`
+
+#       Version
+SCRIPT_NAME=`head -2 ${0} | awk {'printf$2'}`
+SCRIPT_VERSION=`head -2 ${0} | awk {'printf$3'}`
+
+#       Default help and version arguments
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] ; then
+        display_help
         exit 0
 fi
+if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
+        echo "${SCRIPT_NAME} ${SCRIPT_VERSION}"
+        exit 0
+fi
+
+#       INFO
+get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[INFO]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Begin" 1>&2
+
+#       DEBUG
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[DEBUG]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Name_of_command >${0}< Name_of_arg1 >${1}<" 1>&2 ; fi
+
 ###
 TLSUSER=${1:-${USER}}
 USERHOME=${2:-/home/}
-LOCALHOSTNAME=`hostname -f`
-if [ "${DEBUG}" == "1" ] ; then echo -e "> DEBUG ${LINENO} TLSUSER >${TLSUSER}< USERHOME >${USERHOME}< LOCALHOSTNAME >${LOCALHOSTNAME}<" 1>&2 ; fi
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[DEBUG]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  TLSUSER >${TLSUSER}< USERHOME >${USERHOME}< LOCALHOST >${LOCALHOST}<" 1>&2 ; fi
+
 #	Root is required to check other users or user can check their own certs
 if ! [ $(id -u) = 0 -o ${USER} = ${TLSUSER} ] ; then
 	display_help
-	echo "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:   Use sudo ${0}  ${TLSUSER}"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Use sudo ${0}  ${TLSUSER}" 1>&2
 	echo -e "${BOLD}\n>>   SCRIPT MUST BE RUN AS ROOT TO CHECK <another-user>/.docker DIRECTORY. <<\n${NORMAL}"	1>&2
 	exit 1
 fi
+
 #	Check if user has home directory on system
 if [ ! -d ${USERHOME}${TLSUSER} ] ; then 
 	display_help
-	echo -e "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	${TLSUSER} does not have a home directory\n\ton this system or ${TLSUSER} home directory is not ${USERHOME}${TLSUSER}"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  ${TLSUSER} does not have a home directory on this system or ${TLSUSER} home directory is not ${USERHOME}${TLSUSER}" 1>&2
 	exit 1
 fi
+
 #	Check if user has .docker directory
 if [ ! -d ${USERHOME}${TLSUSER}/.docker ] ; then 
 	display_help
-	echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	${TLSUSER} does not have a .docker directory"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  ${TLSUSER} does not have a .docker directory" 1>&2
 	exit 1
 fi
+
 #	Check if user has .docker ca.pem file
 if [ ! -e ${USERHOME}${TLSUSER}/.docker/ca.pem ] ; then 
-	echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	${TLSUSER} does not have a .docker/ca.pem file"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  ${TLSUSER} does not have a .docker/ca.pem file" 1>&2
 	exit 1
 fi
+
 #	Check if user has .docker cert.pem file
 if [ ! -e ${USERHOME}${TLSUSER}/.docker/cert.pem ] ; then 
-	echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	${TLSUSER} does not have a .docker/cert.pem file"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  ${TLSUSER} does not have a .docker/cert.pem file" 1>&2
 	exit 1
 fi
+
 #	Check if user has .docker key.pem file
 if [ ! -e ${USERHOME}${TLSUSER}/.docker/key.pem ] ; then 
-	echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	${TLSUSER} does not have a .docker/key.pem file"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  ${TLSUSER} does not have a .docker/key.pem file" 1>&2
 	exit 1
 fi
+
 #	View user certificate expiration date of ca.pem file
 TEMP=`openssl x509 -in  ${USERHOME}${TLSUSER}/.docker/ca.pem -noout -enddate`
 echo -e "\n${NORMAL}View ${USERHOME}${TLSUSER}/.docker certificate ${BOLD}expiration date of ca.pem ${NORMAL}file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	View user certificate expiration date of cert.pem file
 TEMP=`openssl x509 -in ${USERHOME}${TLSUSER}/.docker/cert.pem -noout -enddate`
 echo -e "\nView ${USERHOME}${TLSUSER}/.docker certificate ${BOLD}expiration date of cert.pem ${NORMAL}file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	View user certificate issuer data of the ca.pem file.
 TEMP=`openssl x509 -in ${USERHOME}${TLSUSER}/.docker/ca.pem -noout -issuer`
 echo -e "\nView ${USERHOME}${TLSUSER}/.docker certificate ${BOLD}issuer data of the ca.pem ${NORMAL}file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	View user certificate issuer data of the cert.pem file.
 TEMP=`openssl x509 -in ${USERHOME}${TLSUSER}/.docker/cert.pem -noout -issuer`
 echo -e "\nView ${USERHOME}${TLSUSER}/.docker certificate ${BOLD}issuer data of the cert.pem ${NORMAL}file:\n\t${BOLD}${TEMP}${NORMAL}"
+
 #	Verify that user public key in your certificate matches the public portion of your private key.
 echo -e "\nVerify that user public key in your certificate matches the public portion\n\tof your private key."
 (cd ${USERHOME}${TLSUSER}/.docker ; openssl x509 -noout -modulus -in cert.pem | openssl md5 ; openssl rsa -noout -modulus -in key.pem | openssl md5) | uniq
 echo -e "${BOLD}WARNING:${NORMAL}  -> If ONLY ONE line of output is returned then the public key\n\tmatches the public portion of your private key.\n"
+
 #	Verify that user certificate was issued by the CA.
 echo -e "Verify that user certificate was issued by the CA:${BOLD}\n"
-openssl verify -verbose -CAfile ${USERHOME}${TLSUSER}/.docker/ca.pem ${USERHOME}${TLSUSER}/.docker/cert.pem  || { echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	User certificate for ${TLSUSER} on ${LOCALHOSTNAME} was NOT issued by CA." ; exit 1; }
+openssl verify -verbose -CAfile ${USERHOME}${TLSUSER}/.docker/ca.pem ${USERHOME}${TLSUSER}/.docker/cert.pem  || { get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  User certificate for ${TLSUSER} on ${LOCALHOST} was NOT issued by CA." ; exit 1; }
+
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker/ca.pem
 echo -e "\n${NORMAL}Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker"
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/ca.pem) != 444 ]; then
-	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	File permissions for ${USERHOME}${TLSUSER}/.docker/ca.pem\n\tare not 444.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/ca.pem) to 0444 file permissions"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  File permissions for ${USERHOME}${TLSUSER}/.docker/ca.pem are not 444.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/ca.pem) to 0444 file permissions" 1>&2
 	chmod 0444 ${USERHOME}${TLSUSER}/.docker/ca.pem
 fi
+
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker/cert.pem
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/cert.pem) != 444 ]; then
-	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	File permissions for ${USERHOME}${TLSUSER}/.docker/cert.pem\n\tare not 444.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/cert.pem) to 0444 file permissions"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  File permissions for ${USERHOME}${TLSUSER}/.docker/cert.pem are not 444.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/cert.pem) to 0444 file permissions" 1>&2
 	chmod 0444 ${USERHOME}${TLSUSER}/.docker/cert.pem
 fi
+
 #	Verify and correct file permissions for ${USERHOME}${TLSUSER}/.docker/key.pem
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/key.pem) != 400 ]; then
-	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	File permissions for ${USERHOME}${TLSUSER}/.docker/key.pem\n\tare not 400.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/key.pem) to 0400 file permissions"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  File permissions for ${USERHOME}${TLSUSER}/.docker/key.pem are not 400.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker/key.pem) to 0400 file permissions" 1>&2
 	chmod 0400 ${USERHOME}${TLSUSER}/.docker/key.pem
 fi
+
 #	Verify and correct directory permissions for ${USERHOME}${TLSUSER}/.docker directory
 if [ $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker) != 700 ]; then
-	echo -e "${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	Directory permissions for ${USERHOME}${TLSUSER}/.docker\n\tare not 700.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker) to 700 directory permissions"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Directory permissions for ${USERHOME}${TLSUSER}/.docker\n\tare not 700.  Correcting $(stat -Lc %a ${USERHOME}${TLSUSER}/.docker) to 700 directory permissions" 1>&2
 	chmod 700 ${USERHOME}${TLSUSER}/.docker
 fi
-#
+
+#	Help hint
 echo -e "\nUse script ${BOLD}create-user-tls.sh${NORMAL} to update user TLS if user TLS certificate\n\thas expired."
-echo -e "\n${0} ${LINENO} [${BOLD}INFO${NORMAL}]:	Done.\n"	1>&2
-#
+ 
 #	May want to create a version of this script that automates this process for SRE tools,
 #		but keep this script for users to run manually,
 #	open ticket and remove this comment
+
+#
+get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[INFO]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Done." 1>&2
 ###
