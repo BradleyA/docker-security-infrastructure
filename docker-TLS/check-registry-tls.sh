5f27224bd4dc249c44f5d9d390d77364bd5a1b20
Bradley Allen <allen.bradley@ymail.com>
2019-03-01 22:11:04 -0600

    need to open issue to create docker-TLS/create-registry-tls.sh

---
+++ docker-TLS/check-registry-tls.sh
@@ -0,0 +1,8 @@
+
+# 	docker-TLS/check-registry-tls.sh  3.130.542  2019-03-01T22:11:03.984554-06:00 (CST)  https://github.com/BradleyA/docker-security-infrastructure-scripts.git  uadmin  six-rpi3b.cptx86.com 3.129  
+# 	   need to open issue to create docker-TLS/create-registry-tls.sh 
+
+
+
+
+
