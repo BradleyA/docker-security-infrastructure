3e25d7fd4dfcc6c3c56958947083f1985bb49638
Bradley Allen <allen.bradley@ymail.com>
2019-03-09 08:26:16 -0600

    begin writing create docker-TLS/check-registry-tls.sh #42

--- docker-TLS/check-registry-tls.sh
+++ docker-TLS/check-registry-tls.sh
@@ -1,15 +1,116 @@
+#!/bin/bash
+# 	docker-TLS/check-registry-tls.sh  3.150.563  2019-03-09T08:26:16.534422-06:00 (CST)  https://github.com/BradleyA/docker-security-infrastructure-scripts.git  uadmin  six-rpi3b.cptx86.com 3.149  
+# 	   begin writing create docker-TLS/check-registry-tls.sh #42 
+### check-registry-tls.sh -
+#       Copyright (c) 2019 Bradley Allen
+#       License is in the online DOCUMENTATION, DOCUMENTATION URL defined below.
 
-# 	docker-TLS/check-registry-tls.sh  3.139.551  2019-03-06T16:05:05.253103-06:00 (CST)  https://github.com/BradleyA/docker-security-infrastructure-scripts.git  uadmin  six-rpi3b.cptx86.com 3.138  
-# 	   set file permission for certs 
-# 	docker-TLS/check-registry-tls.sh  3.130.542  2019-03-01T22:11:03.984554-06:00 (CST)  https://github.com/BradleyA/docker-security-infrastructure-scripts.git  uadmin  six-rpi3b.cptx86.com 3.129  
-# 	   need to open issue to create docker-TLS/create-registry-tls.sh 
-
-###
+#
 echo "In development"
 exit
-
-
 # 
+
+###
+#   production standard 5
+#       Order of precedence: environment variable, default code
+if [ "${DEBUG}" == "" ] ; then DEBUG="0" ; fi   # 0 = debug off, 1 = debug on, 'export DEBUG=1', 'unset DEBUG' to unset environment variable (bash)
+#       set -x
+#       set -v
+BOLD=$(tput -Txterm bold)
+NORMAL=$(tput -Txterm sgr0)
+###
+display_help() {
+echo -e "\n${NORMAL}${0} - brief description"
+echo -e "\nUSAGE\n   ${0} [XX | YY | ZZ]"
+echo    "   ${0} [--file <PATH>/<FILE_NAME> | -f <PATH>/<FILE_NAME>]"
+echo    "   ${0} [--help | -help | help | -h | h | -?]"
+echo    "   ${0} [--version | -version | -v]"
+echo -e "\nDESCRIPTION"
+#       Displaying help DESCRIPTION in English en_US.UTF-8
+echo    "<your help goes here>" 
+echo    ">>> NEED TO COMPLETE THIS SOON, ONCE I KNOW HOW IT IS GOING TO WORK :-) <<<"
+echo -e "\n<<Paragraph two>>"
+#       Displaying help DESCRIPTION in French fr_CA.UTF-8, fr_FR.UTF-8, fr_CH.UTF-8
+if [ "${LANG}" == "fr_CA.UTF-8" ] || [ "${LANG}" == "fr_FR.UTF-8" ] || [ "${LANG}" == "fr_CH.UTF-8" ] ; then
+        echo -e "\n--> ${LANG}"
+        echo    "<votre aide va ici>" # your help goes here
+        echo    "Souhaitez-vous traduire la section description?" # Do you want to translate the description section?
+elif ! [ "${LANG}" == "en_US.UTF-8" ] ; then
+        get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[WARN]${NORMAL}  Your language, ${LANG}, is not supported.  Would you like to translate the description section?" 1>&2
+fi
+echo -e "\nSTORAGE & CERTIFICATION ARCHITECTURE TREE"
+echo    "/usr/local/data/                            <-- <DATA_DIR>"
+echo    "   <CLUSTER>/                               <-- <CLUSTER>"
+echo    "   ├── docker                               <-- Docker image & working"
+echo    "   │                                            directory"
+echo    "   └── docker-registry/                     <-- Docker registry directory"
+echo    "       ├── <REGISTRY_HOST>-<REGISTRY_PORT>/ <-- Registry container mount"
+echo    "       │   │── certs/                       <-- Registry cert directory"
+echo    "       │   │   ├── domain.crt               <-- Registry cert"
+echo    "       │   │   └── domain.key               <-- Registry private key"
+echo    "       │   └── docker/                      <-- Registry storage directory"
+echo -e "       └── <REGISTRY_HOST>-<REGISTRY_PORT>/ <-- Registry container mount\n"
+echo    "/etc/docker/certs.d/                        <-- Host docker cert directory"
+echo    "   ├── <REGISTRY_HOST>:<REGISTRY_PORT>/     <-- Registry cert directory"
+echo    "   │   └── ca.crt                           <-- Daemon trust registry cert"
+echo    "   └── <REGISTRY_HOST>:<REGISTRY_PORT>/     <-- Registry cert directory"
+echo    "       └── ca.crt                           <-- Daemon trust registry cert"
+echo -e "\nENVIRONMENT VARIABLES"
+echo    "If using the bash shell, enter; 'export DEBUG=1' on the command line to set"
+echo    "the DEBUG environment variable to '1' (0 = debug off, 1 = debug on).  Use the"
+echo    "command, 'unset DEBUG' to remove the exported information from the DEBUG"
+echo    "environment variable.  You are on your own defining environment variables if"
+echo    "you are using other shells."
+echo    "   DEBUG       (default '0')"
+echo -e "\n   <<your environment variables information goes here>>"
+echo -e "\nOPTIONS\n   -f, --file\n      path and file on system '<path>/<file_name>'"
+echo -e "\nOPTIONS\n   Option one - description"
+echo    "   XX       xxx xxxxxxx xxx"
+echo    "   YY       xxx xxxxx xxx "
+echo    "   zz       xx xxxxxxx xx"
+echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/   <<URL to online repository README>>"
+echo -e "\nEXAMPLES\n   ${BOLD}${0} <<code example goes here>>${NORMAL}\n\n   <<your code examples description goes here>>\n"
+}
+
+#       Date and time function ISO 8601
+get_date_stamp() {
+DATE_STAMP=$(date +%Y-%m-%dT%H:%M:%S.%6N%:z)
+TEMP=$(date +%Z)
+DATE_STAMP="${DATE_STAMP} (${TEMP})"
+}
+
+#       Fully qualified domain name FQDN hostname
+LOCALHOST=$(hostname -f)
+
+#       Version
+SCRIPT_NAME=$(head -2 "${0}" | awk {'printf $2'})
+SCRIPT_VERSION=$(head -2 "${0}" | awk {'printf $3'})
+
+#       UID and GID
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+#       Added line because USER is not defined in crobtab jobs
+if ! [ "${USER}" == "${LOGNAME}" ] ; then  USER=${LOGNAME} ; fi
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Setting USER to support crobtab...  USER >${USER}<  LOGNAME >${LOGNAME}<" 1>&2 ; fi
+
+#       Default help and version arguments
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] ; then
+        display_help | more
+        exit 0
+fi
+if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
+        echo "${SCRIPT_NAME} ${SCRIPT_VERSION}"
+        exit 0
+fi
+
+#       INFO
+get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[INFO]${NORMAL}  Started..." 1>&2
+
+#       DEBUG
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Name_of_command >${0}< Name_of_arg1 >${1}< Name_of_arg2 >${2}< Name_of_arg3 >${3}<  Version of bash ${BASH_VERSION}" 1>&2 ; fi
+
+###
 
 
 
@@ -22,12 +123,10 @@ echo    "certificates for multiple private registries on the same host.  The"
 echo    "<REGISTRY_HOST> and <REGISTRY_PORT> number is required when copying the"
 echo    "ca.crt into the /etc/docker/certs.d/<REGISTRY_HOST>:<REGISTRY_PORT>/"
 echo    "directory on each host using the private registry."
-echo -e "\nConfiguration"
-echo    "   /usr/local/"
-echo    "   └── docker-registry-<REGISTRY_PORT>/certs/ <-- Certificate directory"
-echo    "      ├── domain.crt                          <-- registry certificate"
-echo    "      └── domain.key                          <-- registry key"
-echo    "   /etc/docker/certs.d/"
-echo    "   └── <REGISTRY_HOST>:<REGISTRY_PORT>/       <-- Certificate directory"
-echo    "      └── ca.crt                              <-- Certificate authority"
+
+
+
+#       Set REGISTRY_HOST variable to host entered during the creation of certificates
+REGISTRY_HOST=$(openssl x509 -in domain.crt -noout -issuer | cut -d '/' -f 7 | cut -d '=' -f 2)
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Variable... REGISTRY_HOST >${REGISTRY_HOST}<" 1>&2 ; fi
 
