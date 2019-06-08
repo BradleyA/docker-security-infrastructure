7b877230275c9553acc2005b6033ca90f8b3d7e0
Bradley Allen <allen.bradley@ymail.com>
2019-06-07 21:29:02 -0500

    docker-TLS/c* - added production standard 8.0 --usage #52

--- docker-TLS/create-new-openssl.cnf-tls.sh
+++ docker-TLS/create-new-openssl.cnf-tls.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	docker-TLS/create-new-openssl.cnf-tls.sh  3.262.729  2019-06-07T21:29:02.335904-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.261  
+# 	   docker-TLS/c* - added production standard 8.0 --usage #52 
 # 	docker-TLS/create-new-openssl.cnf-tls.sh  3.234.679  2019-04-10T23:30:18.635712-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure.git  uadmin  six-rpi3b.cptx86.com 3.233  
 # 	   production standard 6.1.177 Architecture tree 
 ### production standard 3.0 shellcheck
@@ -15,15 +17,20 @@ NORMAL=$(tput -Txterm sgr0)
 ### production standard 7.0 Default variable value
 BACKUP_FILE="/etc/ssl/openssl.cnf-$(date +%Y-%m-%dT%H:%M:%S.%6N%:z)"
 ORIGINAL_FILE="/etc/ssl/openssl.cnf"
-### production standard 0.1.158 --help
-display_help() {
+### production standard 8.0 --usage
+display_usage() {
 echo -e "\n${NORMAL}${0} - Modify /etc/ssl/openssl.conf file"
 echo -e "\nUSAGE"
-echo    "   sudo ${0}"
+echo -e "   sudo ${0}\n"
 echo    "   ${0} [--help | -help | help | -h | h | -?]"
+echo    "   ${0} [--usage | -usage | -u]"
 echo    "   ${0} [--version | -version | -v]"
-echo -e "\nDESCRIPTION"
+}
+### production standard 0.1.158 --help
+display_help() {
+display_usage
 #       Displaying help DESCRIPTION in English en_US.UTF-8
+echo -e "\nDESCRIPTION"
 echo    "This script makes a change to openssl.cnf file which is required for"
 echo    "create-user-tls.sh and create-host-tls.sh scripts.  It must be run as root."
 #       Displaying help DESCRIPTION in French fr_CA.UTF-8, fr_FR.UTF-8, fr_CH.UTF-8
@@ -73,9 +80,13 @@ GROUP_ID=$(id -g)
 if ! [ "${USER}" == "${LOGNAME}" ] ; then  USER=${LOGNAME} ; fi
 if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Setting USER to support crobtab...  USER >${USER}<  LOGNAME >${LOGNAME}<" 1>&2 ; fi
 
-#       Default help and version arguments
+#       Default help, usage, and version arguments
 if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] ; then
         display_help | more
+        exit 0
+fi
+if [ "$1" == "--usage" ] || [ "$1" == "-usage" ] || [ "$1" == "usage" ] || [ "$1" == "-u" ] ; then
+        display_usage | more
         exit 0
 fi
 if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
