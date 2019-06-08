86b05f9554cabed1b22b052a19964895465ddb57
Bradley Allen <allen.bradley@ymail.com>
2019-06-07 20:51:50 -0500

    draft update

--- ssh/check-host-fingerprint.sh
+++ ssh/check-host-fingerprint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# 	ssh/check-host-fingerprint.sh  3.254.721  2019-06-07T20:51:49.621850-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.253-2-gab55146  
+# 	   draft update 
+
+echo -e ">>>>   >>>>   IN DEVELOPMENT   IN DEVELOPMENT   IN DEVELOPMENT   <<<<   <<<<"
+echo -e ">>>>   >>>>   IN DEVELOPMENT   IN DEVELOPMENT   IN DEVELOPMENT   <<<<   <<<<"
+echo -e ">>>>   >>>>               NOT READY FOR PRODUCTION               <<<<   <<<<"
+
 # 	ssh/check-host-fingerprint.sh  3.253.718  2019-06-07T20:41:42.369425-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.252  
 # 	   ssh/check-host-fingerprint.sh first ruff idea 
 
@@ -7,8 +14,6 @@
 #	
 #	rm ssh_host_ed25519_key*
 #	ssh-keygen -t ed25519 -C "[$(date +%Y-%m-%dT%H:%M:%S.%6N%:z) - $(date -d '+52 weeks' +%Y-%m-%dT%H:%M:%S.%6N%:z) $(hostname -f)]" -V +52w1d -f ssh_host_ed25519_key -N "" > ssh_host_ed25519_fingerprint
-
-
 
 ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub
 ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub
--- ssh/check-host-ssh.sh
+++ ssh/check-host-ssh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	ssh/check-host-ssh.sh  3.254.721  2019-06-07T20:51:49.767091-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.253-2-gab55146  
+# 	   draft update 
 # 	ssh/check-host-ssh.sh  3.252.717  2019-06-02T20:24:27.971921-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.251-1-gc608054  
 # 	   debugging 
 # 	ssh/check-host-ssh.sh  3.251.715  2019-06-02T13:56:12.378295-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.250  
@@ -27,8 +29,8 @@ NORMAL=$(tput -Txterm sgr0)
 DEFAULT_CLUSTER="us-tx-cluster-1/"
 DEFAULT_DATA_DIR="/usr/local/data/"
 DEFAULT_KEY_LOCATION="%h/.ssh/authorized_keys"
-### production standard 0.1.166 --help
-display_help() {
+### production standard 8.0 --usage
+display_usage() {
 echo -e "\n${NORMAL}${0} - brief description"
 echo -e "\nUSAGE"
 echo    "   ${0} [XX | YY | ZZ]"
@@ -37,9 +39,14 @@ echo    "   ${0} [<REGISTRY_HOST>]"
 echo    "   ${0}  <REGISTRY_HOST> [<REGISTRY_PORT>]"
 echo    "   ${0}  <REGISTRY_HOST>  <REGISTRY_PORT> [<CLUSTER>]"
 echo    "   ${0}  <REGISTRY_HOST>  <REGISTRY_PORT>  <CLUSTER> [<DATA_DIR>]"
-echo    "   ${0}  <REGISTRY_HOST>  <REGISTRY_PORT>  <CLUSTER>  <DATA_DIR> [<SYSTEMS_FILE>]"
+echo -e "   ${0}  <REGISTRY_HOST>  <REGISTRY_PORT>  <CLUSTER>  <DATA_DIR> [<SYSTEMS_FILE>]\n"
 echo    "   ${0} [--help | -help | help | -h | h | -?]"
+echo    "   ${0} [--usage | -usage | -u]"
 echo    "   ${0} [--version | -version | -v]"
+}
+### production standard 0.1.166 --help
+display_help() {
+display_usage
 #       Displaying help DESCRIPTION in English en_US.UTF-8
 echo -e "\nDESCRIPTION"
 echo    "<your help goes here>"
@@ -238,9 +245,13 @@ GROUP_ID=$(id -g)
 if ! [ "${USER}" == "${LOGNAME}" ] ; then  USER=${LOGNAME} ; fi
 if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Setting USER to support crobtab...  USER >${USER}<  LOGNAME >${LOGNAME}<" 1>&2 ; fi
 
-#       Default help and version arguments
+#       Default help, usage, and version arguments
 if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] ; then
         display_help | more
+        exit 0
+fi
+if [ "$1" == "--usage" ] || [ "$1" == "-usage" ] || [ "$1" == "usage" ] || [ "$1" == "-u" ] ; then
+        display_usage | more
         exit 0
 fi
 if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
