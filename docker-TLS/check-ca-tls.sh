39e9649147d1eb950b620bee5c07b42fe4210f56
Bradley Allen <allen.bradley@ymail.com>
2019-06-09 14:23:33 -0500

    created new tool docker-TLS/check-ca-tls.sh,  need to add production standards

---
+++ docker-TLS/check-ca-tls.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 	docker-TLS/check-ca-tls.sh  3.275.742  2019-06-09T14:23:33.079346-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.274  
+# 	   created new tool docker-TLS/check-ca-tls.sh,  need to add production standards 
+
+CA_CERT="ca.pem"
+CA_PRIVATE_CERT="ca-priv-key.pem"
+
+if [ -s ${CA_CERT} ] ; then
+	#       Get certificate start and expiration date of ${CA_CERT} file
+	CA_CERT_START_DATE=$(openssl x509 -in "${CA_CERT}" -noout -startdate | cut -d '=' -f 2)
+	CA_CERT_START_DATE_2=$(date -u -d"${CA_CERT_START_DATE}" +%g%m%d%H%M.%S)
+	CA_CERT_START_DATE=$(date -u -d"${CA_CERT_START_DATE}" +%Y-%m-%dT%H:%M:%S%z)
+	CA_CERT_EXPIRE_DATE=$(openssl x509 -in "${CA_CERT}" -noout -enddate | cut -d '=' -f 2)
+	CA_CERT_EXPIRE_DATE=$(date -u -d"${CA_CERT_EXPIRE_DATE}" +%Y-%m-%dT%H:%M:%S%z)
+	cp -f -p "${CA_CERT}" "${CA_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}"
+	chmod 0444 "${CA_CERT}" "${CA_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}"
+	touch -m -t "${CA_CERT_START_DATE_2}" "${CA_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}" "${CA_CERT}"
+	ls -l ${CA_CERT}*
+else
+	echo "${CA_CERT} not found in this directory $(pwd)"
+fi
+
+#	if [ -z ${CA_PRIVATE_CERT} ] ; then
+#	cp -f -p ".private/${CA_PRIVATE_CERT}" ".private/${CA_PRIVATE_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}"
+#	touch 0400 ".private/${CA_PRIVATE_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}"
+#	fi
