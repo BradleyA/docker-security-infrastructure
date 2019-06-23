0a578e17c5df87088a3fde9142b9fb173cc3947b
Bradley Allen <allen.bradley@ymail.com>
2019-06-23 10:02:31 -0500

    add production standards

--- docker-TLS/check-ca-tls.sh
+++ docker-TLS/check-ca-tls.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
+# 	docker-TLS/check-ca-tls.sh  3.286.753  2019-06-23T10:02:31.122517-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.285  
+# 	   add production standards 
 # 	docker-TLS/check-ca-tls.sh  3.285.752  2019-06-21T21:27:32.676784-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.284  
 # 	   check-ca-tls.sh usage #56 
-# 	docker-TLS/check-ca-tls.sh  3.283.750  2019-06-10T23:51:10.701588-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.282  
-# 	   docker-TLS/check-ca-tls.sh - complete design - in development #56 
-# 	docker-TLS/check-ca-tls.sh  3.276.743  2019-06-09T14:26:35.259926-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.275  
-# 	   marked in development 
 # 	docker-TLS/check-ca-tls.sh  3.275.742  2019-06-09T14:23:33.079346-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.274  
 # 	   created new tool docker-TLS/check-ca-tls.sh,  need to add production standards 
 echo -e ">>>>   >>>>   IN DEVELOPMENT   IN DEVELOPMENT   IN DEVELOPMENT   <<<<   <<<<"
@@ -25,19 +23,153 @@ NORMAL=$(tput -Txterm sgr0)
 ### production standard 7.0 Default variable value
 DEFAULT_CERTDIR="${HOME}/.docker"
 DEFAULT_CA_CERT="ca.pem"
-DEFAULT_CA_PRIVATE_CERT="ca-priv-key.pem"
 ### production standard 8.0 --usage
 display_usage() {
 echo -e "\n${NORMAL}${0} - read start and end date of ${DEFAULT_CA_CERT}"
 echo -e "\nUSAGE"
 echo    "   ${0} [<CERTDIR>]"
-echo    "   ${0}  <CERTDIR> [<CA_CERT>]"
-echo -e "   ${0}  <CERTDIR>  <CA_CERT> [<CA_PRIVATE_CERT>]\n"
+echo -e "   ${0}  <CERTDIR> [<CA_CERT>]\n"
 echo    "   ${0} [--help | -help | help | -h | h | -?]"
 echo    "   ${0} [--usage | -usage | -u]"
 echo    "   ${0} [--version | -version | -v]"
 }
+### production standard 0.1.166 --help
+display_help() {
+display_usage
+#       Displaying help DESCRIPTION in English en_US.UTF-8
+echo -e "\nDESCRIPTION"
+echo    "<your help goes here>"
+echo    ">>> NEED TO COMPLETE THIS SOON, ONCE I KNOW HOW IT IS GOING TO WORK :-) <<<    |"
 
+echo -e "\n<<Paragraph two>>"
+
+echo -e "\nThe <DATA_DIR>/<CLUSTER>/<SYSTEMS_FILE> includes one FQDN or IP address per"
+echo    "line for all hosts in the cluster.  Lines in <SYSTEMS_FILE> that begin with a"
+echo    "'#' are comments.  The <SYSTEMS_FILE> is used by markit/find-code.sh,"
+echo    "Linux-admin/cluster-command/cluster-command.sh, docker-TLS/copy-registry-tls.sh,"
+echo    "pi-display/create-message/create-display-message.sh, and other scripts.  A"
+echo    "different <SYSTEMS_FILE> can be entered on the command line or environment"
+echo    "variable."
+
+echo -e "\nThis script works for the local host only.  To use check-host-tls.sh on a"
+echo    "remote hosts (one-rpi3b.cptx86.com) with ssh port of 12323 as uadmin user;"
+echo -e "\t${BOLD}ssh -tp 12323 uadmin@one-rpi3b.cptx86.com 'sudo check-host-tls.sh'${NORMAL}"
+
+echo    "To loop through a list of hosts in the cluster use,"
+echo    "https://github.com/BradleyA/Linux-admin/tree/master/cluster-command"
+echo -e "\t${BOLD}cluster-command.sh special 'sudo check-host-tls.sh'${NORMAL}"
+
+echo -e "\nThe administration user may receive password and/or passphrase prompts from a"
+echo    "remote systen; running the following may stop the prompts in your cluster."
+echo -e "\t${BOLD}ssh-copy-id <TLS_USER>@<REMOTE_HOST>${NORMAL}"
+echo    "or"
+echo -e "\t${BOLD}ssh-copy-id <TLS_USER>@<192.168.x.x>${NORMAL}"
+echo    "If that does not resolve the prompting challenge then review the man pages for"
+echo    "ssh-agent and ssh-add before entering the following in a terminal window."
+echo -e "\t${BOLD}eval \$(ssh-agent)${NORMAL}"
+echo -e "\t${BOLD}ssh-add${NORMAL}"
+### production standard 4.0 Documentation Language
+#       Displaying help DESCRIPTION in French fr_CA.UTF-8, fr_FR.UTF-8, fr_CH.UTF-8
+if [ "${LANG}" == "fr_CA.UTF-8" ] || [ "${LANG}" == "fr_FR.UTF-8" ] || [ "${LANG}" == "fr_CH.UTF-8" ] ; then
+        echo -e "\n--> ${LANG}"
+        echo    "<votre aide va ici>" # your help goes here
+        echo    "Souhaitez-vous traduire la section description?" # Do you want to translate the description section?
+elif ! [ "${LANG}" == "en_US.UTF-8" ] ; then
+        get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[WARN]${NORMAL}  Your language, ${LANG}, is not supported.  Would you like to translate the description section?" 1>&2
+fi
+echo -e "\nENVIRONMENT VARIABLES"
+echo    "If using the bash shell, enter; 'export DEBUG=1' on the command line to set"
+echo    "the DEBUG environment variable to '1' (0 = debug off, 1 = debug on).  Use the"
+echo    "command, 'unset DEBUG' to remove the exported information from the DEBUG"
+echo    "environment variable.  You are on your own defining environment variables if"
+echo    "you are using other shells."
+echo    "   DEBUG           (default off '0')"
+echo    "   CERTDIR         Certification directory (default '${DEFAULT_CERTDIR}')"
+echo    "   CA_CERT         Name of CA certificate (default '${DEFAULT_CA_CERT}')"
+echo -e "\nOPTIONS"
+echo    "Order of precedence: CLI options, environment variable, default code."
+echo    "   CERTDIR         Certification directory (default '${DEFAULT_CERTDIR}')"
+echo    "   CA_CERT         Name of CA certificate (default '${DEFAULT_CA_CERT}')"
+### production standard 6.1.177 Architecture tree
+echo -e "\nARCHITECTURE TREE"   # STORAGE & CERTIFICATION
+echo    "<USER_HOME>/                               <-- Location of user home directory"
+echo    "└── <USER-1>/.docker/                      <-- User docker cert directory"
+echo    "    ├── ca.pem                             <-- Symbolic link to user tlscacert"
+echo    "    └── registry-certs-<REGISTRY_HOST>-<REGISTRY_PORT>/ <-- Working directory"
+echo    "        │                                      to create registory certs"
+echo -e "        └── ca.crt                         <-- Daemon registry domain cert"
+echo    "/etc/ "
+echo    "└── docker/ "
+echo    "    └── certs.d/                           <-- Host docker cert directory"
+echo    "        ├── daemon/                        <-- Daemon cert directory"
+echo    "        │   └── ca.pem                     <-- Daemon tlscacert"
+echo    "        └── <REGISTRY_HOST>:<REGISTRY_PORT>/ < Registry cert directory"
+echo -e "            └── ca.crt                     <-- Daemon registry domain cert\n"
+echo -e "\nDOCUMENTATION"
+echo    "   https://github.com/BradleyA/   <<URL to online repository README>>"
+echo -e "\nEXAMPLES"
+echo -e "   <<your code examples description goes here>>\n\t${BOLD}${0} <<code example goes here>>${NORMAL}"
+echo -e "   <<your code examples description goes here>>\n\t${BOLD}${0}${NORMAL}"
+}
+
+#       Date and time function ISO 8601
+get_date_stamp() {
+DATE_STAMP=$(date +%Y-%m-%dT%H:%M:%S.%6N%:z)
+TEMP=$(date +%Z)
+DATE_STAMP="${DATE_STAMP} (${TEMP})"
+}
+
+#       Fully qualified domain name FQDN hostname
+LOCALHOST=$(hostname -f)
+
+#       Version
+SCRIPT_NAME=$(head -2 "${0}" | awk {'printf $2'})
+SCRIPT_VERSION=$(head -2 "${0}" | awk {'printf $3'})
+
+#       UID and GID
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+#       Added line because USER is not defined in crobtab jobs
+if ! [ "${USER}" == "${LOGNAME}" ] ; then  USER=${LOGNAME} ; fi
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Setting USER to support crobtab...  USER >${USER}<  LOGNAME >${LOGNAME}<" 1>&2 ; fi
+
+#       Default help, usage, and version arguments
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] ; then
+        display_help | more
+        exit 0
+fi
+if [ "$1" == "--usage" ] || [ "$1" == "-usage" ] || [ "$1" == "usage" ] || [ "$1" == "-u" ] ; then
+        display_usage | more
+        exit 0
+fi
+if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
+        echo "${SCRIPT_NAME} ${SCRIPT_VERSION}"
+        exit 0
+fi
+
+### production standard 2.0 log format (WHEN WHERE WHAT Version Line WHO UID:GID [TYPE] Message)
+#       INFO
+get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[INFO]${NORMAL}  Started..." 1>&2
+
+#       DEBUG
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Name_of_command >${0}< Name_of_arg1 >${1}< Name_of_arg2 >${2}< Name_of_arg3 >${3}<  Version of bash ${BASH_VERSION}" 1>&2 ; fi
+
+###
+#       Root is required to copy certs
+if ! [ $(id -u) = 0 ] ; then
+        display_help | more
+        get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[ERROR]${NORMAL}  Use sudo ${0}" 1>&2
+#       Help hint
+        echo -e "\n\t${BOLD}>>   SCRIPT MUST BE RUN AS ROOT   <<\n${NORMAL}"    1>&2
+        exit 1
+fi
+
+### production standard 7.0 Default variable value
+#       Order of precedence: CLI argument, environment variable, default code
+if [ $# -ge  1 ]  ; then CERTDIR=${1} ; elif [ "${CERTDIR}" == "" ] ; then CERTDIR=${DEFAULT_CERTDIR} ; fi
+if [ $# -ge  2 ]  ; then CA_CERT=${2} ; elif [ "${CA_CERT}" == "" ] ; then CA_CERT=${DEFAULT_CA_CERT} ; fi
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Variable... CERTDIR >${CERTDIR}< CA_CERT >${CA_CERT}<" 1>&2 ; fi
 
 #	argument over ride default $HOME/.docker    -   DEFAULT_CERTDIR="/etc/docker/certs.d/daemon/"
 
@@ -57,7 +189,6 @@ else
 	echo "${CERTDIR}/${CA_CERT} not found in this directory $(pwd)"
 fi
 
-#	if [ -z ${CA_PRIVATE_CERT} ] ; then
-#	cp -f -p ".private/${CA_PRIVATE_CERT}" ".private/${CA_PRIVATE_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}"
-#	touch 0400 ".private/${CA_PRIVATE_CERT}_${CA_CERT_START_DATE}_${CA_CERT_EXPIRE_DATE}"
-#	fi
+#
+get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[INFO]${NORMAL}  Operation finished." 1>&2
+###
