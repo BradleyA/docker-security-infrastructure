d0b9a1765f2c7ae0699dfaae15ff2e6a2d8fa28a
Bradley Allen <allen.bradley@ymail.com>
2019-07-27 23:42:59 -0500

    testing .git/hooks/pre- post- commit and FVT-option-help- test cases

--- ssh/check-user-ssh.sh
+++ ssh/check-user-ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	ssh/check-user-ssh.sh  3.415.886  2019-07-27T23:40:47.360888-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.414  
+# 	ssh/check-user-ssh.sh  3.416.887  2019-07-27T23:42:59.289468-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.415  
 # 	   testing .git/hooks/pre- post- commit and FVT-option-help- test cases 
 ### production standard 3.0 shellcheck
 ### production standard 5.1.160 Copyright
