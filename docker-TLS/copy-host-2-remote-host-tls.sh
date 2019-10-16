92617ad81d1f42cbc55d81e67e47c49512fc8d54
Bradley Allen <allen.bradley@ymail.com>
2019-10-15 23:31:11 -0500

    docker-TLS/copy-host-2-remote-host-tls.sh  - add code so localhost does not use scp & ssh #48 first pass

--- docker-TLS/copy-host-2-remote-host-tls.sh
+++ docker-TLS/copy-host-2-remote-host-tls.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.457.961  2019-10-13T21:15:58.035732-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure.git  uadmin  five-rpi3b.cptx86.com 3.456-2-g59e591e  
-# 	   #64 docker-TLS/copy-user-2-remote-host-tls.sh   Production standard 2.3.529 log format, 8.3.530 --usage, 1.3.531 DEBUG variable 
-# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.456.958  2019-10-13T20:56:21.738318-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure.git  uadmin  five-rpi3b.cptx86.com 3.455-2-gd3bc5e6  
-# 	   docker-TLS/copy-host-2-remote-host-tls.sh   close #65  Production standard 2.3.529 log format, 8.3.530 --usage, 1.3.531 DEBUG variable 
-# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.280.747  2019-06-10T12:43:31.498537-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.279  
-# 	   update copy-host-2-remote-host-tls.sh while trying to reproduce incident docker-TLS/check-{host,user}-tls.sh - which one should check if the ca.pem match #49 
+# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.463.976  2019-10-15T23:31:11.161027-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure.git  uadmin  five-rpi3b.cptx86.com 3.462-1-g2ea495b  
+# 	   docker-TLS/copy-host-2-remote-host-tls.sh  - add code so localhost does not use scp & ssh #48 first pass 
+# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.457.961  2019-10-13T21:15:58.035732-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure.git  uadmin  five-rpi3b.cptx86.com 3.456-2-g59e591e
+# 	   #64 docker-TLS/copy-user-2-remote-host-tls.sh   Production standard 2.3.529 log format, 8.3.530 --usage, 1.3.531 DEBUG variable
+# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.456.958  2019-10-13T20:56:21.738318-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure.git  uadmin  five-rpi3b.cptx86.com 3.455-2-gd3bc5e6
+# 	   docker-TLS/copy-host-2-remote-host-tls.sh   close #65  Production standard 2.3.529 log format, 8.3.530 --usage, 1.3.531 DEBUG variable
+# 	docker-TLS/copy-host-2-remote-host-tls.sh  3.280.747  2019-06-10T12:43:31.498537-05:00 (CDT)  https://github.com/BradleyA/docker-security-infrastructure  uadmin  six-rpi3b.cptx86.com 3.279
+# 	   update copy-host-2-remote-host-tls.sh while trying to reproduce incident docker-TLS/check-{host,user}-tls.sh - which one should check if the ca.pem match #49
 #86# docker-TLS/copy-host-2-remote-host-tls.sh - Copy public, private keys and CA to remote host
 ###  Production standard 3.0 shellcheck
 ###  Production standard 5.1.160 Copyright
@@ -132,7 +134,7 @@ LOCALHOST=$(hostname -f)
 #    Version
 #    Assumptions for the next two lines of code:  The second line in this script includes the script path & name as the second item and
 #    the script version as the third item separated with space(s).  The tool I use is called 'markit'. See example line below:
-#       template/template.sh  3.517.783  2019-09-13T18:20:42.144356-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.516  
+#       template/template.sh  3.517.783  2019-09-13T18:20:42.144356-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.516
 SCRIPT_NAME=$(head -2 "${0}" | awk '{printf $2}')
 SCRIPT_VERSION=$(head -2 "${0}" | awk '{printf $3}')
 if [[ "${SCRIPT_NAME}" == "" ]] ; then SCRIPT_NAME="${0}" ; fi
@@ -204,85 +206,115 @@ echo -e "\n\t${BOLD}${USER}${NORMAL} user may receive password and passphrase pr
 echo -e "\tfrom ${REMOTE_HOST}.  Running"
 echo -e "\t${BOLD}ssh-copy-id ${USER}@${REMOTE_HOST}${NORMAL}"
 echo -e "\tmay stop some of the prompts.\n"
-if $(ssh "${REMOTE_HOST}" 'exit' >/dev/null 2>&1 ) ; then
-#    Check if /etc/docker directory on ${REMOTE_HOST}
+if [[ "${LOCALHOST}" != "${REMOTE_HOST}" ]] ; then  #  >>> #48 Not "${LOCALHOST}"
+  if ! $(ssh "${REMOTE_HOST}" 'exit' >/dev/null 2>&1 ) ; then
+    display_help | more
+    new_message "${LINENO}" "ERROR" "  ${REMOTE_HOST} not responding on ssh port." 1>&2
+    exit 1
+  fi
+  #    Check if /etc/docker directory on ${REMOTE_HOST}
   if ! $(ssh -t "${REMOTE_HOST}" "test -d /etc/docker") ; then
     new_message "${LINENO}" "ERROR" "  /etc/docker directory missing, is docker installed on ${REMOTE_HOST}." 1>&2
     exit 1
   fi
+else
+  #    Check if /etc/docker directory on ${LOCALHOST}
+  if ! [[ -d "/etc/docker" ]] ; then
+    new_message "${LINENO}" "ERROR" "  /etc/docker directory missing, is docker installed on ${REMOTE_HOST}." 1>&2
+    exit 1
+  fi
+fi
 
 #    Create working directory ${WORKING_DIRECTORY}/${REMOTE_HOST}
-  mkdir -p "${REMOTE_HOST}"
-  cd "${REMOTE_HOST}"
+mkdir -p "${REMOTE_HOST}"
+cd       "${REMOTE_HOST}"
 
 #    Backup ${REMOTE_HOST}:${CERT_DAEMON_DIR}/..
-  FILE_DATE_STAMP=$(date +%Y-%m-%dT%H.%M.%S.%6N%z)
-  echo -e "\n\tBacking up ${REMOTE_HOST}:${CERT_DAEMON_DIR}/.."
-  echo -e "\tto $(pwd)\n\tRoot access required.\n"
+FILE_DATE_STAMP=$(date +%Y-%m-%dT%H.%M.%S.%6N%z)
+echo -e "\n\tBacking up ${REMOTE_HOST}:${CERT_DAEMON_DIR}/.."
+echo -e "\tto $(pwd)\n\tRoot access required.\n"
+if [[ "${LOCALHOST}" != "${REMOTE_HOST}" ]] ; then  #  >>> #48 Not "${LOCALHOST}"
   ssh -t "${USER}@${REMOTE_HOST}" "sudo mkdir -p ${CERT_DAEMON_DIR} ; cd /etc ; sudo tar -pcf /tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar ./docker/certs.d/daemon ; sudo chown ${USER}.${USER} /tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar ; chmod 0400 /tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
   scp -p "${USER}@${REMOTE_HOST}:/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" .
   ssh -t "${USER}@${REMOTE_HOST}" "rm -f /tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
-  tar -pxf "${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+else
+#    Backup ${CERT_DAEMON_DIR}/..
+  sudo mkdir -p "${CERT_DAEMON_DIR}"
+  cd /etc
+  sudo tar -pcf "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" ./docker/certs.d/daemon
+  sudo chown "${USER}"."${USER}"  "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+  chmod 0400 "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+  cp -p      "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" .
+  rm -f      "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+fi
 
-#    Check if /etc/docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem file exists on remote system
-  if [[ -e "./docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem" ]] ; then
-    echo -e "\n\t/etc/docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem"
-    echo -e "\talready exists, ${BOLD}renaming existing keys${NORMAL} so new keys can be installed.\n"
-    mv "./docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem" "./docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem_${FILE_DATE_STAMP}"
-    mv "./docker/certs.d/daemon/${REMOTE_HOST}-cert.pem" "./docker/certs.d/daemon/${REMOTE_HOST}-cert.pem_${FILE_DATE_STAMP}"
-    mv "./docker/certs.d/daemon/${CA_CERT}" "./docker/certs.d/daemon/${CA_CERT}_${FILE_DATE_STAMP}"
-    rm ./docker/certs.d/daemon/{cert,key}.pem
-  fi
+tar -pxf "${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+
+#    Check if /etc/docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem file exists
+if [[ -e "./docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem" ]] ; then
+  echo -e "\n\t/etc/docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem"
+  echo -e "\talready exists, ${BOLD}renaming existing keys${NORMAL} so new keys can be installed.\n"
+  mv "./docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem" "./docker/certs.d/daemon/${REMOTE_HOST}-priv-key.pem_${FILE_DATE_STAMP}"
+  mv "./docker/certs.d/daemon/${REMOTE_HOST}-cert.pem" "./docker/certs.d/daemon/${REMOTE_HOST}-cert.pem_${FILE_DATE_STAMP}"
+  mv "./docker/certs.d/daemon/${CA_CERT}" "./docker/certs.d/daemon/${CA_CERT}_${FILE_DATE_STAMP}"
+  rm ./docker/certs.d/daemon/{cert,key}.pem
+fi
 
 #    Create certification tar file and install it to ${REMOTE_HOST}
-  chmod 0700 ./docker/certs.d/daemon
-  cp -p "../${REMOTE_HOST}-priv-key.pem" ./docker/certs.d/daemon
-  cp -p "../${REMOTE_HOST}-cert.pem" ./docker/certs.d/daemon
-  cp -p ../${CA_CERT} ./docker/certs.d/daemon
-  cd ./docker/certs.d/daemon
-  ln -s "${REMOTE_HOST}-priv-key.pem" key.pem
-  ln -s "${REMOTE_HOST}-cert.pem" cert.pem
-  cd ../../..
-  FILE_DATE_STAMP=$(date +%Y-%m-%dT%H.%M.%S.%6N%z)
-  tar -pcf "./${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" ./docker/certs.d/daemon
-  chmod 0600 "./${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+chmod 0700 ./docker/certs.d/daemon
+cp -p "../${REMOTE_HOST}-priv-key.pem" ./docker/certs.d/daemon
+cp -p "../${REMOTE_HOST}-cert.pem" ./docker/certs.d/daemon
+cp -p ../${CA_CERT} ./docker/certs.d/daemon
+cd ./docker/certs.d/daemon
+ln -s "${REMOTE_HOST}-priv-key.pem" key.pem
+ln -s "${REMOTE_HOST}-cert.pem" cert.pem
+cd ../../..
+FILE_DATE_STAMP=$(date +%Y-%m-%dT%H.%M.%S.%6N%z)
+tar -pcf "./${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" ./docker/certs.d/daemon
+chmod 0600 "./${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+if [[ "${LOCALHOST}" != "${REMOTE_HOST}" ]] ; then  #  >>> #48 Not "${LOCALHOST}"
   scp -p "./${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" "${USER}@${REMOTE_HOST}:/tmp"
+else
+  cp -p "./${REMOTE_HOST}-${FILE_DATE_STAMP}.tar" /tmp
+fi
 
 #    Create remote directory /etc/docker/certs.d/daemon
 #    This directory was selected to place dockerd TLS certifications because
 #    docker registry stores it's TLS certifications in /etc/docker/certs.d.
-  echo -e "\n\tCopying dockerd certification to ${REMOTE_HOST}"
-  echo -e "\tRoot access required.\n"
+echo -e "\n\tCopying dockerd certification to ${REMOTE_HOST}"
+echo -e "\tRoot access required.\n"
+if [[ "${LOCALHOST}" != "${REMOTE_HOST}" ]] ; then  #  >>> #48 Not "${LOCALHOST}"
   ssh -t "${USER}@${REMOTE_HOST}" "cd /etc ; sudo tar -pxf /tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar ; sudo chmod 0700 /etc/docker ; sudo chmod 0700 /etc/docker/certs.d ; sudo chown -R root.root ./docker ; rm /tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
-  cd ..
+else
+  cd /etc
+  sudo tar -pxf "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+  sudo chmod 0700 /etc/docker
+  sudo chmod 0700 /etc/docker/certs.d
+  sudo chown -R root.root ./docker
+  rm "/tmp/${REMOTE_HOST}-${FILE_DATE_STAMP}.tar"
+fi
+cd ..
 
 #    Remove working directory ${WORKING_DIRECTORY}/${REMOTE_HOST}
-  rm -rf "${REMOTE_HOST}"
+rm -rf "${REMOTE_HOST}"
 
 #    Display instructions about certification environment variables
-  echo -e "\n\tAdd TLS flags to dockerd so it will know to use TLS certifications"
-  echo -e "\t(--tlsverify, --tlscacert, --tlscert, --tlskey).  Scripts that will"
-  echo -e "\thelp with setup and operations of Docker using TLS can be found:"
-  echo    "https://github.com/BradleyA/docker-security-infrastructure/tree/master/dockerd-configuration-options"
-  echo -e "\tThe dockerd-configuration-options scripts will help with configuration"
-  echo -e "\tof dockerd on systems running Ubuntu 16.04 (systemd) and Ubuntu 14.04"
-  echo -e "\t(Upstart)."
+echo -e "\n\tAdd TLS flags to dockerd so it will know to use TLS certifications"
+echo -e "\t(--tlsverify, --tlscacert, --tlscert, --tlskey).  Scripts that will"
+echo -e "\thelp with setup and operations of Docker using TLS can be found:"
+echo    "https://github.com/BradleyA/docker-security-infrastructure/tree/master/dockerd-configuration-options"
+echo -e "\tThe dockerd-configuration-options scripts will help with configuration"
+echo -e "\tof dockerd on systems running Ubuntu 16.04 (systemd) and Ubuntu 14.04"
+echo -e "\t(Upstart)."
 #
-  echo -e "\n\tIf dockerd is already using TLS certifications then entering one of the"
-  echo -e "\tfollowing will restart dockerd with the new certifications.\n"
-  echo -e "\tUbuntu 16.04 (Systemd) ${BOLD}ssh -t ${REMOTE_HOST} 'sudo systemctl restart docker'${NORMAL}"
-  echo -e "\tUbuntu 14.04 (Upstart) ${BOLD}ssh -t ${REMOTE_HOST} 'sudo service docker restart'${NORMAL}"
-  new_message "${LINENO}" "INFO" "  Operation finished..." 1>&2
-  exit 0
-else
-  display_help | more
-  new_message "${LINENO}" "ERROR" "  ${REMOTE_HOST} not responding on ssh port." 1>&2
-  exit 1
-fi
+echo -e "\n\tIf dockerd is already using TLS certifications then entering one of the"
+echo -e "\tfollowing will restart dockerd with the new certifications.\n"
+echo -e "\tUbuntu 16.04 (Systemd) ${BOLD}ssh -t ${REMOTE_HOST} 'sudo systemctl restart docker'${NORMAL}"
+echo -e "\tUbuntu 14.04 (Upstart) ${BOLD}ssh -t ${REMOTE_HOST} 'sudo service docker restart'${NORMAL}"
 
 # >>>	May want to create a version of this script that automates this process for SRE tools,
 # >>>	but keep this script for users to run manually,
-# >>>	open ticket and remove this comment 
+# >>>	open ticket and remove this comment
 
 #
 new_message "${LINENO}" "INFO" "  Operation finished....." 1>&2
